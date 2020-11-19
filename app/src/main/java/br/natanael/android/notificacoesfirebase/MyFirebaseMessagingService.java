3fc9cd07ac9fa280ec87a417f47cba91b51828e1
Natanael <natanaelsantosbr@gmail.com>
2020-11-18 21:44:51 -0300

    Notificacao Simples

---
+++ app/src/main/java/br/natanael/android/notificacoesfirebase/MyFirebaseMessagingService.java
@@ -0,0 +1,21 @@
+package br.natanael.android.notificacoesfirebase;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.i("Notificacao","Notificacao recebida");
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+    }
+}
