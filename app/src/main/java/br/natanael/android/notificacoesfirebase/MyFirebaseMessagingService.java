b980ffa47740351a95e2521f555ae6edbd142427
Natanael <natanaelsantosbr@gmail.com>
2020-11-18 22:09:13 -0300

    Criar Notificação Recebida

--- app/src/main/java/br/natanael/android/notificacoesfirebase/MyFirebaseMessagingService.java
+++ app/src/main/java/br/natanael/android/notificacoesfirebase/MyFirebaseMessagingService.java
@@ -1,17 +1,61 @@
 package br.natanael.android.notificacoesfirebase;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
-    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
-        Log.i("Notificacao","Notificacao recebida");
+    public void onMessageReceived(@NonNull RemoteMessage notificacao) {
+        if(notificacao.getNotification() != null)
+        {
+            String titulo = notificacao.getNotification().getTitle();
+            String corpo = notificacao.getNotification().getBody();
+
+            enviarNotificacao(titulo, corpo);
+        }
+    }
+
+    private void enviarNotificacao(String titulo, String corpo) {
+        String canal = getString(R.string.default_notification_channel_id);
+        Uri uriSom = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Intent i = new Intent(this, NotificacoesActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_ONE_SHOT);
+
+        //Criar notificação
+        NotificationCompat.Builder notificacao = new NotificationCompat.Builder(this, canal)
+                .setContentTitle(titulo)
+                .setContentText(corpo)
+                .setSmallIcon(R.drawable.ic_camera_black_24dp)
+                .setSound(uriSom)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        //Recupera notificationManager
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Verifica versao do android a partir dooreo para configurar canal de notificacao
+        if(Build.VERSION.SDK_INT  >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel = new NotificationChannel(canal,"canal", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        //Envia notificacao
+        notificationManager.notify(0, notificacao.build());
     }
 
     @Override
---
+++ app/src/main/java/br/natanael/android/notificacoesfirebase/NotificacoesActivity.java
@@ -0,0 +1,14 @@
+package br.natanael.android.notificacoesfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificacoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificacoes);
+    }
+}
